13a9af637bd558a6ceb3fa822d647232b8599748
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2025-08-15 19:39:17 +0200

    fix: get rid of auth middleware

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 
 import { AuthController } from 'modules/auth/auth.controller';
-import { authMiddleware } from 'modules/auth/auth.middleware';
 
 const controller = new AuthController();
 export const authRouter = Router();
 
 authRouter.post('/register', controller.register.bind(controller));
 authRouter.post('/login', controller.login.bind(controller));
-authRouter.get('/me', authMiddleware, controller.me.bind(controller));
+authRouter.get('/me', controller.me.bind(controller));
